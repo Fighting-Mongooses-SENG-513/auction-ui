3be1195ead56d2c38f1d864cd5bfbfcd53e6d63e
Aden McCann <aden.mccann@gmail.com>
2020-04-15 16:06:04 -0600

    Added auctioneer image upload functionality

--- src/app/auctioneer-dialog/auctioneer-add-item-dialog.component.ts
+++ src/app/auctioneer-dialog/auctioneer-add-item-dialog.component.ts
@@ -1,6 +1,8 @@
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Component, Inject } from '@angular/core';
 import { AuctionItem } from '../models/auction-item.model';
+//import {HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-auctioneer-add-item-dialog',
@@ -11,14 +13,28 @@ export class AuctioneerAddItemDialogComponent {
   tagsList: string[] = ['Automotive', 'Books', 'Clothing', 'Electronics', 'Jewelry', 'Kitchen', 'Movies', 'Music', 'Sports'];
 
   missingFields = false;
+  imagePointer: File = null;
+  clientID: string = 'dd65a136a77f9f3';
+  uploadUrl: string = 'https://api.imgur.com/3/image';
 
   constructor(
-    public dialogRef: MatDialogRef<AuctioneerAddItemDialogComponent>,
+    public dialogRef: MatDialogRef<AuctioneerAddItemDialogComponent>, private http:HttpClient,
     @Inject(MAT_DIALOG_DATA) public data: AuctionItem) {
     }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  async onFileSelected(event) {
+    this.imagePointer = event.target.files[0];
+    const formData = new FormData();
+    formData.append('image', this.imagePointer, this.imagePointer.name);
+
+    let header = new HttpHeaders({"authorization": 'Client-ID '+this.clientID});
+
+    const imageData = await this.http.post(this.uploadUrl, formData, {headers:header}).toPromise();
+    this.data.imageUrl = imageData['data'].link;
   }
 
   addItem() {
